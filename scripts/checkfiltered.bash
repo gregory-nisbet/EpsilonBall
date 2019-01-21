c6214cd59c52d9e0b401182c5ae557633cb209d6
Gregory Nisbet <gregory.nisbet@gmail.com>
2019-01-21 15:20:42 -0800

    short message on successful test run

--- scripts/checkfiltered.bash
+++ scripts/checkfiltered.bash
@@ -4,14 +4,13 @@ set -o pipefail
 
 test -d ./src || exit 10
 
-# 1. delete uninteresting lines
-# 2. capitalize status words like "passed"
-stack 'test' 2>&1 | awk '
-/^$/           { next; }
-/^\[\]$/       { next; }
-/^\[\".*\"\]$/ { next; }
+# TODO: better convention for temporary file
+outfile=/tmp/testoutput.$$
 
-1 == 1 {
-  print $0;
-}
-' | sed 's/passed/PASSED/'
+if stack 'test' &> "$outfile"; then
+  printf 'PASS\n'
+else
+  cat <"$outfile"
+  # TODO: preserve exit status
+  exit 10
+fi
